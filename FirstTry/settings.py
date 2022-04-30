ab83b09f3c6c9b3210e0c399b08e84582d43bb32
Sovatiano <dovn666@gmail.com>
2022-04-30 15:39:33 +0300

    Problem with choice field fixed

--- FirstTry/settings.py
+++ FirstTry/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l1z66cb%&5bps-x*=vvrq2=(cqu7lbv(u&4tx6t%(6g3-(9*ka'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['medlgallery.herokuapp.com', '127.0.0.1']
 # ALLOWED_HOSTS = []
