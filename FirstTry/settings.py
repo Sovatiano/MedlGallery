4fb83b03f84b47a121aa703b045ed67fc127d73e
Sovatiano <dovn666@gmail.com>
2022-09-18 16:01:40 +0300

    First Commit1

--- FirstTry/settings.py
+++ FirstTry/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l1z66cb%&5bps-x*=vvrq2=(cqu7lbv(u&4tx6t%(6g3-(9*ka
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['medlgallery.herokuapp.com', '127.0.0.1']
+# ALLOWED_HOSTS = ['medlgallery.herokuapp.com', '127.0.0.1']
 # ALLOWED_HOSTS = []
 # Application definition
 
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'Europe/Moscow'
 
